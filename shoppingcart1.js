cd45864fcd6ed4d5846b3949a3964380427eeb29
Kiara Patrick <215726227+kiaracode3@users.noreply.github.com>
2025-07-20 22:42:19 -0700

    did shopping cart pt 2 assignment

--- shoppingcart1.js
+++ shoppingcart1.js
@@ -3,3 +3,35 @@ const storeProds = {
     carrot: 4,
     fries: 5,
 }; 
+
+const storeItemsDiv = document.getElementById("store-items");
+const cartList = document.getElementById("cart");
+const cartTotal = document.getElementById("cart-total");
+let total = 0;
+
+for (const item in storeProds){
+    const button = document.createElement("button"); 
+    button.textContent = item;
+    storeItemsDiv.appendChild(button);
+
+    function addToGroceryList () {
+    const li = document.createElement("li");
+    li.textContent = item;
+    cartList.appendChild(li); 
+    };
+
+    button.addEventListener("click", addToGroceryList); 
+    
+    function cartTotalFunction (){
+    total = total + storeProds[item];
+    cartTotal.innerHTML = total;
+    }    
+    
+    button.addEventListener("click", cartTotalFunction);
+    
+
+};
+
+
+
+
